92a76d07efdef2b078741406a4668006d5af596c
Alicja <akrzeminska1519@gmail.com>
2020-05-29 10:45:18 +0200

    core: Created accounts base template

--- social_media_site/settings.py
+++ social_media_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Frontend
+    'bootstrap4',
     # My apps
     'accounts',
 ]
